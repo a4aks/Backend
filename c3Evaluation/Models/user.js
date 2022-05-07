9446b78ea9bd4a6eb1288fe4f5414a41da969d08
a4aks <a4ashish04@gmail.com>
2022-05-07 15:19:30 +0530

    databse connection

---
+++ c3Evaluation/Models/user.js
@@ -0,0 +1,13 @@
+
+const mongoose = require('mongoose');
+
+const user = new mongoose.Schema({
+    "firstName": { type: String, required: true },
+    "lastName": { type: String },
+    "age": { type: Number, required: true },
+    "email": { type: String, required: true },
+    "profileImages" : [{type: String}],
+    "timestamps": { type: String, required: true }
+});
+
+module.exports = mongoose.model('user', user);
